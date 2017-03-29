3174641e6515276dfaf482e6a1c454e52105a5dc
amlcurran <aml.curran@gmail.com>
2017-03-29 22:38:57 +0200

    Initial Commit

---
+++ Novoda Issues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Novoda Issues
+//
+//  Created by Alex Curran on 29/03/2017.
+//  Copyright © 2017 Alex Curran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
